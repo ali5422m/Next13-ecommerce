16c631b10d25e1e766a237f80b18b95ceb8bfdca
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-09-02 23:31:48 +0330

    create productDetails and productId

---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,26 @@
+import Container from "@/app/components/Container";
+import { products } from "@/utils/products";
+import ProductDetails from "@/app/product/[productId]/productDetails";
+
+interface IPrams {
+    productId?: string;
+}
+
+const Product = ({ params }: { params: IPrams }) => {
+
+    const product = products.find((item) => item.id === params.productId)
+
+    return (
+        <div className="p-8">
+            <Container>
+                <ProductDetails product={product} />
+                <div className="flex flex-col mt-20 gap-4">
+                    <div>Add Rating</div>
+                    <ListRating product={product}/>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Product;
