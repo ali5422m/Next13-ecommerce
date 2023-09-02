1c9260746e27543973e42542e90fbc0d89fc692d
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-09-02 23:40:17 +0330

    create ListRating

---
+++ app/product/[productId]/ListRating.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+
+import { Avatar, Rating } from "@mui/material";
+import moment from "moment";
+
+interface ListRatingProps {
+    product: any;
+}
+
+const ListRating: React.FC<ListRatingProps> = ({ product }) => {
+    return (
+        <div>
+            <Heading title="Product Review" />
+            <div className="text-sm mt-2">
+                {product.reviews &&
+                    product.reviews.map((review: any) => {
+                        return (
+                            <div key={review.id} className="max-w-[300px]">
+                                <div className="flex gap-2 items-center">
+                                    <Avatar src={review.user.image}/>
+                                    <div className="font-semibold">{review?.user.name}A</div>
+                                    <div>{moment(review.createdDate).fromNow()}</div>
+                                </div>
+                                <div className="mt-2">
+                                    <Rating value={review.rating} readOnly/>
+                                    <div className="ml-2">{review.comment}</div>
+                                    <hr className="mb-4 mt-4"/>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
+        </div>
+    );
+};
+
+export default ListRating;
