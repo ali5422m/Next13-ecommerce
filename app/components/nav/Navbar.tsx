80676efe659a9cc89638e086eecfc0f89bf93df5
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-09-02 12:14:39 +0330

    create Navbar

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Container from "@/app/components/Container";
+import Link from "next/link";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({ subsets: ["latin"], weight:["400"]});
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="sticky top-0 w-full bg-slate-700 z-30 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    <div className="flex items-center justify-between gap-3 md:gap-0">
+                        <Link className={`${redressed.className} font-bold text-2xl`} href='/'>
+                            E-Commerece Shop
+                        </Link>
+                        <div className="hidden md:block mainContainer">Search</div>
+                        <div className="flex items-center gap-8 md:gap-12">
+                            <CartCount/>
+                            <div>UserMenu</div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
         </div>
     );
 };
