268a2c21f7175bed3d7c8798deb7c203ca313552
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-09-05 00:51:01 +0330

    add CartProvider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type {Metadata} from 'next'
 import {Poppins} from 'next/font/google'
 import Navbar from "@/app/components/nav/Navbar";
 import Footer from "@/app/components/footer/Footer";
+import {Toaster} from "react-hot-toast";
+import CartProvider from "@/providers/CartProvider";
 
 const poppins = Poppins({subsets: ['latin'], weight: ['400', '700']});
 
@@ -19,13 +21,21 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={poppins.className}>
-        <div className="flex flex-col min-h-screen">
-            <Navbar/>
-            <main className="flex-grow">
-                {children}
-            </main>
-            <Footer/>
-        </div>
+        <Toaster
+            toastOptions={{
+                style: {
+                    background: "rgb(51 65 85)",
+                    color: "#fff"
+                }
+            }}
+        />
+        <CartProvider>
+            <div className='flex flex-col min-h-screen'>
+                <Navbar/>
+                <main className='flex-grow'>{children}</main>
+                <Footer/>
+            </div>
+        </CartProvider>
         </body>
         </html>
     )
